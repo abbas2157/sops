c598555cbc848def1a325ba01a0169422d51e862
Mudassar Abbas <mudassar@zoomdigital.ae>
2024-04-25 18:15:29 +0500

    Trainer (Model,migration,Controller and views)

---
+++ database/migrations/2024_04_25_130829_create_trainers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trainers', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_id');
+            $table->string('highest_qualification')->nullable();
+            $table->string('years_of_experience')->nullable();
+            $table->string('areas_of_expertise')->nullable();
+            $table->string('curriculum_vitae')->nullable();
+            $table->integer('created_by');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trainers');
+    }
+};
