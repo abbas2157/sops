4feefd56a8bfdb220429ef0458e8965c88ea63a7
Mudassar Abbas <mudassar@zoomdigital.ae>
2024-05-08 19:46:10 +0500

    Comments Setup

---
+++ database/migrations/2024_05_08_144301_create_comments_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->text('text')->nullable();
+            $table->string('user_name')->nullable();
+            $table->integer('course_id')->nullable();
+            $table->integer('step_id')->nullable();
+            $table->integer('user_id')->nullable();
+            $table->enum('show',[0,1])->default(0);
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('comments');
+    }
+};
