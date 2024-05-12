34e8a2ed0b6eb7e53041a9ec34e3c9f7e1fa9489
Mudassar Abbas <abbas8156@gmail.com>
2024-05-12 22:18:38 +0500

    add filters on comments and reviews

--- resources/views/admin/comments/index.blade.php
+++ resources/views/admin/comments/index.blade.php
@@ -9,6 +9,40 @@
         <div class="border-bottom">
             <h3 class="all-adjustment text-center pb-2 mb-0">Comments</h3>
         </div>
+        <div class="card card-shadow border-0 mt-4 rounded-3 mb-3 p-3">
+            <div class="row">
+                <div class="col-md-3">
+                    <div class="form-group fw-bold">
+                        <label for="course">Select Course</label>
+                        <select class="form-control form-select subheading mt-2" name="course" id="course" required>
+                            @if($courses->isNotEmpty())
+                                <option disabled selected> Select Course</option>
+                                @foreach($courses as $course)
+                                    <option value="{{ $course->id ?? '' }}" {{ ($course->id == request()->course) ? 'selected' : '' }}>{{ $course->name ?? '' }}</option>
+                                @endforeach
+                            @endif
+                        </select>
+                    </div>
+                </div>
+                <div class="col-md-3">
+                    <div class="form-group fw-bold">
+                        <label for="trainee">Select User</label>
+                        <select class="form-control form-select subheading mt-2" name="trainee" id="trainee" required>
+                            @if($trainees->isNotEmpty())
+                                <option disabled selected> Select User</option>
+                                @foreach($trainees as $trainee)
+                                    <option value="{{ $trainee->id ?? '' }}" {{ ($trainee->id == request()->trainee) ? 'selected' : '' }}>{{ $trainee->name ?? '' }}</option>
+                                @endforeach
+                            @endif
+                        </select>
+                    </div>
+                </div>
+                <div class="col-md-2 pt-3">
+                    <button id="search" class="btn save-btn text-white mt-3">Search</button>
+                    <button id="clear" class="btn warning-btn text-white mt-3">Clear</button>
+                </div>
+            </div>
+        </div>
         <div class="card card-shadow border-0 mt-4 rounded-3 mb-3">
             <div class="card-header bg-white border-0 rounded-3">
                 <div class="row my-3">
@@ -71,7 +105,7 @@
                                                     </a>
                                                 @else
                                                     <a class="dropdown-item" href="{{ route('admin.comments.show', $comment->id) }}?show=1">
-                                                        <img src="{{ asset('assets/img/content-right-arrow.svg') }}" class="img-fluid me-1" style="width: 17%;" alt=""/> Publish 
+                                                        <img src="{{ asset('assets/img/content-right-arrow.svg') }}" class="img-fluid me-1" style="width: 17%;" alt=""/> Publish
                                                     </a>
                                                 @endif
                                                 <a class="dropdown-item" href="javascript:;">
@@ -103,4 +137,25 @@
     </div>
 @stop
 @section('js')
+<script>
+    $( document ).ready(function() {
+        $('#search').click(function(){
+            var url = '?';
+                if ($('#course').val() != '' &&  $('#course').val() != undefined && $('#trainee').val() != '' &&  $('#trainee').val() != undefined) {
+                 url += 'course='+$('#course').val() + '&' + 'trainee='+$('#trainee').val();
+                }else if($('#course').val() != '' &&  $('#course').val() != undefined){
+                    url += 'course='+$('#course').val();
+
+
+                }else if($('#trainee').val() != '' &&  $('#trainee').val() != undefined){
+                    url += 'trainee='+$('#trainee').val()
+                }
+
+            window.location.replace('{{ route("admin.comments.index") }}' +  url);
+        })
+    });
+    $(document).on("click", "#clear", function (e) {
+        window.location.replace('{{ route("admin.comments.index") }}');
+    });
+</script>
 @stop
