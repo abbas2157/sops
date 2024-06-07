755012f66b8958026de04679342013569df36733
Mudassar Abbas <mudassar@zoomdigital.ae>
2024-06-07 18:51:31 +0500

    Admin

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -10,6 +10,7 @@ return Application::configure(basePath: dirname(__DIR__))
         commands: __DIR__.'/../routes/console.php',
         health: '/up',
         then: function () {
+            Route::middleware('web')->group(base_path('routes/admin.php'));
             Route::middleware('web')->group(base_path('routes/trainee.php'));
             Route::middleware('web')->group(base_path('routes/trainer.php'));
         },
