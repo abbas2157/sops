7e3e036a04df97dfdd64ce7a0a123c9e69078fc3
Mudassar Abbas <mudassar@zoomdigital.ae>
2024-11-07 15:50:29 +0500

    change

--- config/app.php
+++ config/app.php
@@ -65,7 +65,7 @@ return [
     |
     */
 
-    'timezone' => env('APP_TIMEZONE', 'Asia/Karachi'),
+    'timezone' => env('APP_TIMEZONE', 'Asia/Karachi' ),
 
     /*
     |--------------------------------------------------------------------------
