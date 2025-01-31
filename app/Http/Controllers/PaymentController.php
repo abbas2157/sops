b44d165fbbb86f2c1c90bc84f077e0774f3e2fc9
Mudassar Abbas <mudassar@zoomdigital.ae>
2025-01-31 10:19:24 +0500

    Payment error

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\{Cookie, Auth};
+use Illuminate\Support\Facades\{Cookie, Auth, DB};
 use App\Models\{Course, Payment, Coupon, JoinedCourse, Trainee};
 use Carbon\Carbon;
 
@@ -87,8 +87,8 @@ class PaymentController extends Controller
                     $payment->total_price = $course->price;
                     $payment->save();
                 }
-                // Cookie::queue(Cookie::forget('course'));
-                // return redirect('trainee');
+                Cookie::queue(Cookie::forget('course'));
+                return redirect('trainee');
             }
             DB::commit();
             return abort(404);
